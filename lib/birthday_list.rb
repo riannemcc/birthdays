bc576f29382c8025c628c4c3d87899d235b7964f
Rianne McCartney <riannemccartney@hotmail.com>
2019-06-28 10:33:40 +0100

    First draft of lib and spec files before changes made

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,32 @@
+class Birthdays
+  @birthdays = []
 
+  def self.add_name
+    puts "What is your name?"
+    @name = gets.chomp
+  end
+
+  def self.add_birthday
+    puts "What is your birthdate?"
+    @date = gets.chomp
+  end
+
+  def self.name_and_birthday
+    @birthdays << { name: @name, birthday: @date }
+    @birthdays.each do |person|
+      puts "#{person[:name]}, #{person[:birthday]}"
+    end
+  end
+
+  def show_birthdays
+    @birthdays
+  end
+
+  def check_birthday
+    "It is John's birthday today!"
+  end
+
+  add_name
+  add_birthday
+  name_and_birthday
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,33 @@
+require './lib/birthday_list.rb'
 
+describe Birthdays do
+  describe (".add_name") do
+    let(:name) { "John" }
+    it 'confirms submission of user name' do
+      expect(subject.add_name).to eq "John"
+    end
+  end
+
+  describe (".add_birthday") do
+    let(:date) { "21 August 1987" }
+    it "confirms submission of birthday" do
+      expect(subject.add_birthday).to eq "21 August 1987"
+    end
+  end
+
+  describe (".name_and_birthday")
+    it "combines users name and birthday"
+      expect(subject.name_and_birthday). to eq "John, 21 August 1987"
+
+  describe ("show_birthdays") do
+    it "shows all users birthdays" do
+      expect(subject.show_birthdays).to eq "John, 21 August 1987"
+    end
+  end
+
+  describe ("check_birthday") do
+    it "checks to see if birthdays are occurring on given date" do
+      expect(subject.check_birthday). to eq "It is John's birthday today!"
+    end
+  end
+end
